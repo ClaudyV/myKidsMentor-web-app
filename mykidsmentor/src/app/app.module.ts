8349557fb7c23f2e7cc7b2ac4cb90aec8dcf0d88
Claude Vernet Michel <claudevernetmichel22@gmail.com>
2020-09-07 00:10:58 +0800

    navbar desktop done 100%/ navbar mobile done 95%

--- mykidsmentor/src/app/app.module.ts
+++ mykidsmentor/src/app/app.module.ts
@@ -3,9 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// components generated
 import { NavbarComponent } from './navbar/navbar.component';
 import { BodyComponent } from './body/body.component';
+
+// imported modules
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +26,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- mykidsmentor/src/app/navbar/navbar.component.ts
+++ mykidsmentor/src/app/navbar/navbar.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  searchKey: string; // Searck input
+
   constructor() { }
 
   ngOnInit(): void {
+    const mkmHamburger = document.querySelector('.mkm-hamburger');
+    const mkmNavLinks = document.querySelector('.mkm-nav-links');
+    const links = document.querySelectorAll('.mkm-nav-links li');
+
+    mkmHamburger.addEventListener('click', () => {
+      mkmNavLinks.classList.toggle('open');
+      mkmHamburger.classList.toggle('toggle');
+      links.forEach(link => {
+
+        link.classList.toggle('fade');
+      });
+    });
+  }
+
+  // This function clears the search input as soon as users click on close button
+  onSearchClear() {
+    this.searchKey = ''; // Empty the user input when you click on close button
   }
 
 }
