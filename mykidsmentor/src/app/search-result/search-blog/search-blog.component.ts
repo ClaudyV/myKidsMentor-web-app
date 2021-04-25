5a7627118ce7bf8cee9f0a0d642f5a2757bfe09c
Claude Vernet Michel <claudevernetmichel22@gmail.com>
2021-04-25 18:47:26 +0800

    Home Page (Search Development Done)

--- mykidsmentor/src/app/search-result/search-blog/search-blog.component.ts
+++ mykidsmentor/src/app/search-result/search-blog/search-blog.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from 'src/app/service/shared.service';
+import { Observable } from 'rxjs';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-blog',
@@ -8,8 +11,17 @@ import { SharedService } from 'src/app/service/shared.service';
 })
 export class SearchBlogComponent implements OnInit {
 
-  constructor(private sharedServe: SharedService) {
+  isSmallScreen: Observable<boolean>;
+  shareSmallScreen: Observable<boolean>;
+  articleActionBigScreen: Observable<boolean>;
+  articleActionSmallScreen: Observable<boolean>;
+  
+  constructor(private sharedServe: SharedService,
+              breakpointObserver: BreakpointObserver) {
     this.sharedServe.setQuotesValue(false);
+    this.isSmallScreen = breakpointObserver.observe('(max-width: 985px)').pipe(map(result => !result.matches));
+    this.articleActionBigScreen = breakpointObserver.observe('(max-width: 1400px)').pipe(map(result => !result.matches));
+    this.articleActionSmallScreen = breakpointObserver.observe('(min-width: 1401px)').pipe(map(result => !result.matches));
    }
 
   ngOnInit(): void {
